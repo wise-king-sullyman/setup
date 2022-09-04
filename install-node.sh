8c259a944a963ac65f5e9a60f26d0f0fc5bc14be
Sully <wise.king.sullyman@gmail.com>
2022-09-04 12:46:34 -0400

    Add node installation

---
+++ install-node.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+nvm install --lts
+
+nvm use --lts
+
+node -v
+
+read -p "Node installed if a version number was printed above, press enter to continue"
--- setup.sh
+++ setup.sh
@@ -6,3 +6,7 @@ source git-config-setup.sh
 
 source ssh-key-setup.sh
 
+source install-nvm-fedora.sh
+
+source install-node.sh
+
