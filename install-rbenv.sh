6e96c3a1ef761249b50132de100cdd7025bf949f
Sully <wise.king.sullyman@gmail.com>
2022-09-07 10:51:34 -0400

    Update rbenv installation confirmation message

--- install-rbenv.sh
+++ install-rbenv.sh
@@ -4,4 +4,4 @@ mkdir -p "$(rbenv root)"/plugins
 git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
 
 rbenv -v
-read -p "rbenv is installed if a rbenv version is returned above. Press enter to install Ruby"
+read -p "rbenv is installed if a rbenv version is returned above. Press enter to continue"
