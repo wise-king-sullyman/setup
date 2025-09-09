41b50d1a615860b5ae63efc8f5aa9d0a812f12ee
wise-king-sullyman <wise.king.sullyman@gmail.com>
2025-09-09 17:13:59 -0400

    Add option to set vscode as the default commit editor

--- git-config-setup.sh
+++ git-config-setup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-git config --global user.name $username
-git config --global user.email $email
-git config --global init.defaultBranch main
-git config --global color.ui auto
+# git config --global user.name $username
+# git config --global user.email $email
+# git config --global init.defaultBranch main
+# git config --global color.ui auto
+
+read -p "set vscode as your default commit editor? y/n " update_commit_editor
+
+if [[ $update_commit_editor = "y" ]]; then
+  git config --global core.editor "code --wait"
+fi
+
 echo git configuration completed
