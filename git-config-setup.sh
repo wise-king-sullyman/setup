f687e03f23644fd67a597b7ba3d0245491752b13
wise-king-sullyman <wise.king.sullyman@gmail.com>
2025-09-09 17:16:13 -0400

    Reenable general config options

--- git-config-setup.sh
+++ git-config-setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# git config --global user.name $username
-# git config --global user.email $email
-# git config --global init.defaultBranch main
-# git config --global color.ui auto
+git config --global user.name $username
+git config --global user.email $email
+git config --global init.defaultBranch main
+git config --global color.ui auto
 
 read -p "set vscode as your default commit editor? y/n " update_commit_editor
 
